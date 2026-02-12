e17a6406d5c5e0c8a85fd211d15c50f19c0ae520
austinbeaufort1 <austinbeaufort11@gmail.com>
2026-02-11 23:19:44 -0500

    fixing import

--- src/components/Table/getTagColor.tsx
+++ src/components/Table/getTagColor.tsx
@@ -5,36 +5,36 @@ import {
   difficultyTagColors,
   angleTagColors,
   AngleTagColors,
-} from "../../types/uicolors";
+} from "../../types/uiColors";
 
 export function getDifficultyTag(text: DifficultyDesc): DifficultyTagColors {
   return text === "Easy"
     ? difficultyTagColors.easy
     : text === "Moderate"
-    ? difficultyTagColors.moderate
-    : text === "Moderately Strenuous"
-    ? difficultyTagColors.moderatelyStrenuous
-    : text === "Strenuous"
-    ? difficultyTagColors.strenous
-    : text === "Very Strenuous"
-    ? difficultyTagColors.veryStrenuous
-    : text === "Challenging"
-    ? difficultyTagColors.challenging
-    : difficultyTagColors.bomber;
+      ? difficultyTagColors.moderate
+      : text === "Moderately Strenuous"
+        ? difficultyTagColors.moderatelyStrenuous
+        : text === "Strenuous"
+          ? difficultyTagColors.strenous
+          : text === "Very Strenuous"
+            ? difficultyTagColors.veryStrenuous
+            : text === "Challenging"
+              ? difficultyTagColors.challenging
+              : difficultyTagColors.bomber;
 }
 
 export function getAngleTag(angle: AngleDesc): AngleTagColors {
   return angle === "Flat" || angle == "Nearly Flat"
     ? angleTagColors.flat
     : angle === "Gentle Slopes"
-    ? angleTagColors.nearlyFlat
-    : angle === "Moderate"
-    ? angleTagColors.gentleSlope
-    : angle === "Moderately Steep"
-    ? angleTagColors.moderatelySteep
-    : angle === "Steep"
-    ? angleTagColors.steep
-    : angle === "Very Steep"
-    ? angleTagColors.verySteep
-    : angleTagColors.terrifying;
+      ? angleTagColors.nearlyFlat
+      : angle === "Moderate"
+        ? angleTagColors.gentleSlope
+        : angle === "Moderately Steep"
+          ? angleTagColors.moderatelySteep
+          : angle === "Steep"
+            ? angleTagColors.steep
+            : angle === "Very Steep"
+              ? angleTagColors.verySteep
+              : angleTagColors.terrifying;
 }
